4423b22ff816130ed99ad076e0dcf964148bcb64
Lynn Dixon <ldixon@redhat.com>
2018-11-29 02:08:24 -0500

    another attempt

--- live_sectional_daemon.py
+++ live_sectional_daemon.py
@@ -10,6 +10,15 @@ from daemon import runner
 
 import live_sectional
 
-app = live_sectional.App()
+class App():
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/null'
+        self.stderr_path = '/dev/null'
+        self.pidfile_path =  '/var/run/livesectional.pid'
+        self.pidfile_timeout = 5
+    run()
+
+app = App()
 daemon_runner = runner.DaemonRunner(app)
 daemon_runner.do_action()
